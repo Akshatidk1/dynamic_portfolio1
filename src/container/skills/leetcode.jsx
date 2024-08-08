68cee6f42a29da6a2eb56eec263499582e9ce21a
Akshat Kumar Nishad <akshatkumarnishad@gamil.com>
2024-08-08 16:57:28 +0530

    leetcode

--- src/container/skills/leetcode.jsx
+++ src/container/skills/leetcode.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-
-import { urlFor, client } from "../../client";
+import "./Skills.scss";
 
 const Leetcode = () => {
-  const [leetcode, setleetcode] = useState([]);
+  const [leetcode, setLeetcode] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "leetcodeIcons"]';
-
-    client.fetch(query).then((data) => {
-      setleetcode(data);
-    });
+    // Fetch the JSON file from the public folder
+    fetch('/leetcodeData.json')
+      .then(response => response.json())
+      .then(data => setLeetcode(data))
+      .catch(error => console.error('Error fetching leetcode data:', error));
   }, []);
 
   return (
-    <div className="app__skills-acc-leet">
+    <div className="app__skills-acc-leet aligned-img-container">
       {leetcode.map((code) => (
         <motion.div
           whileHover={{ scale: [1, 1.05] }}
@@ -23,7 +22,7 @@ const Leetcode = () => {
           className="app__skills-acc-leet-item"
           key={code.name}
         >
-          <img src={urlFor(code.icon)} alt={code.name} />
+          <img className={code.name == 'Python' ? 'width-75' : ''} src={code.icon} alt={code.name} />
           <p className="p-text">{code.name}</p>
         </motion.div>
       ))}
